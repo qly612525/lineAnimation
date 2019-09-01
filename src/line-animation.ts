b1553e435e4d536f8cadfb00b8f7877004999c2c
qian.leyi <qianly@foxmail.com>
2019-09-02 00:22:16 +0800

    chore(feat): add Vector class and rotate utils

--- src/line-animation.ts
+++ src/line-animation.ts
@@ -1,4 +1,86 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+
+export default class Vector {
+  /**
+   * degree to radian function
+   * @param degree degree value
+   */
+  static degreeToRadian(degree: number): number {
+    return degree * (Math.PI / 180.0)
+  }
+
+  /**
+   * radian to degree function
+   * @param radian radian value
+   */
+  static radianToDegree(radian: number): number {
+    return radian * (180.0 / Math.PI)
+  }
+
+  private _value = [0.0, 0.0]
+
+  /**
+   * create Vector object.
+   * @constructor
+   * @param nums vector element
+   */
+  constructor(nums: number[]) {
+    this._value = nums
+  }
+
+  /**
+   * get vector value
+   */
+  public get value(): number[] {
+    return this._value
+  }
+
+  /**
+   * caclcuate vector rotateTo special vector need's radian
+   * @param v anthor Vector
+   */
+  public rotateTo(v: Vector): number {
+    const originMod = this.mod()
+    const targetMod = v.mod()
+
+    const dotProduce = this.dotProduce(v)
+    const crossProduce = this.crossProduce2D(v)
+
+    if (dotProduce === 0) return 0
+    else {
+      if (crossProduce > 0) return Math.acos(dotProduce / (originMod * targetMod))
+      else if (crossProduce < 0) return -1 * Math.acos(dotProduce / (originMod * targetMod))
+      else return 0
+    }
+  }
+
+  /**
+   * calculate vectors dot produce
+   * @param v another Vector
+   */
+  public dotProduce(v: Vector): number {
+    const originV = this._value
+    const targetV = v.value
+    return originV[0] * targetV[0] + originV[1] * targetV[1]
+  }
+
+  /**
+   * calculate 2D vectors cross produce
+   * @param v another Vector
+   */
+  public crossProduce2D(v: Vector): number {
+    const originV = this._value
+    const targetV = v.value
+    return originV[0] * targetV[1] - originV[1] * targetV[0]
+  }
+
+  /**
+   * calculate vector's length
+   */
+  public mod(): number {
+    const value = this._value
+    return Math.sqrt(value[0] * value[0] + value[1] * value[1])
+  }
+}
--- test/line-animation.test.ts
+++ test/line-animation.test.ts
@@ -1,14 +1,53 @@
-import DummyClass from '../src/line-animation'
+import Vector from '../src/line-animation'
 
 /**
- * Dummy test
+ * 向量旋转角计算测试
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+describe('Vector class.', () => {
+  it('new vector should be get value.', () => {
+    const v = new Vector([0.0, 1.0])
+    expect(v.value).toEqual([0.0, 1.0])
+  })
+})
+
+/**
+ * 向量旋转角计算测试
+ */
+describe('Vector Rotate Calculate.', () => {
+  it('vector b (1.0, 1.0) rotate to vector a (0.0, 1.0) should be 45 degree.', () => {
+    const va = new Vector([0.0, 1.0])
+    const vb = new Vector([1.0, 1.0])
+    expect(vb.rotateTo(va)).toBeCloseTo(Vector.degreeToRadian(45))
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('vector a (0.0, 1.0) rotate to vector b (1.0, 1.0) should be -45 degree.', () => {
+    const va = new Vector([0.0, 1.0])
+    const vb = new Vector([1.0, 1.0])
+    expect(va.rotateTo(vb)).toBeCloseTo(Vector.degreeToRadian(-45))
+  })
+
+  it('vector b (1.0, -1.0) rotate to vector a (0.0, 1.0) should be 135 degree.', () => {
+    const va = new Vector([0.0, 1.0])
+    const vb = new Vector([1.0, -1.0])
+    expect(vb.rotateTo(va)).toBeCloseTo(Vector.degreeToRadian(135))
+  })
+
+  it('vector a (1.0, 1.0) rotate to vector b (1.0, 1.0) should be 0 degree.', () => {
+    const va = new Vector([1.0, 1.0])
+    const vb = new Vector([1.0, 1.0])
+    expect(va.rotateTo(vb)).toBeCloseTo(Vector.degreeToRadian(0))
+  })
+})
+
+/**
+ * 工具类函数测试
+ */
+describe('utils test.', () => {
+  it('The radian of 45 degree should be close to radian value 0.785398', () => {
+    expect(Vector.degreeToRadian(45)).toBeCloseTo(0.785398)
+  })
+
+  it('The degree of 0.785398 radian should be close to degree value 45', () => {
+    expect(Vector.radianToDegree(0.785398)).toBeCloseTo(45)
   })
 })
